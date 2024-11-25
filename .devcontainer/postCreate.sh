35354856fdab573d2296fd7d36d78474660d8d12
Nicholas A. Brown <nickpowersys@users.noreply.github.com>
2024-11-25 19:45:41 +0000

    Change pip install to uv pip install in postreate

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,5 +4,8 @@
 python3 -m pip install ipython
 
 # avoid using dynamic versioning by grabbing the version from pyproject.toml
+#POETRY_DYNAMIC_VERSIONING_BYPASS="$(yq '.tool.poetry.version' pyproject.toml)" \
+#  python3 -m pip install -e '.[duckdb,clickhouse,examples,geospatial]'
+
 POETRY_DYNAMIC_VERSIONING_BYPASS="$(yq '.tool.poetry.version' pyproject.toml)" \
-  python3 -m pip install -e '.[duckdb,clickhouse,examples,geospatial]'
+    uv pip install -e '.[duckdb,clickhouse,examples,geospatial]'
