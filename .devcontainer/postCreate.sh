de5a6f285b83fb4e005f9cfb715f59440e959c70
Nicholas A. Brown <nickpowersys@users.noreply.github.com>
2024-11-26 01:14:05 +0000

    Clean up postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-# install ibis
-python3 -m pip install ipython
-
-# avoid using dynamic versioning by grabbing the version from pyproject.toml
-#POETRY_DYNAMIC_VERSIONING_BYPASS="$(yq '.tool.poetry.version' pyproject.toml)" \
-#  python3 -m pip install -e '.[duckdb,clickhouse,examples,geospatial]'
-
-POETRY_DYNAMIC_VERSIONING_BYPASS="$(yq '.tool.poetry.version' pyproject.toml)" \
-    uv pip install -e '.[duckdb,clickhouse,examples,geospatial]'
+# Install additional dependencies
+uv pip install ipython
